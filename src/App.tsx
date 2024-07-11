b766ee4db58b63c5f646023c526c926f2cd50af4
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-07-11 22:52:42 +0900

    Chore: 기반 스타일 작성, svg 컴포넌트화 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import ColorTest from "./ColorTest";
+import { LogoSymbol } from "./IconData";
 
 function App() {
     return (
         <div className="font-body p-10 bg-gray-50 w-full h-[100vh]">
+            <LogoSymbol className="w-[30px] h-[32px]" />
             <ColorTest />
         </div>
     );
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import svgr from "@svgr/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), svgr()],
     css: {
         postcss: {
             plugins: [tailwindcss, autoprefixer],
