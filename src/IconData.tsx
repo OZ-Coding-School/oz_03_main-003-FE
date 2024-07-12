20f9fc46574624c49443fd2a67eea6d85df78685
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-07-12 20:49:13 +0900

    Fix: 신규 아이콘 추가, CONFLICT 수정 #4

--- src/IconData.tsx
+++ src/IconData.tsx
@@ -17,7 +17,7 @@ import { ReactComponent as IconAngry } from "./assets/icon-angry.svg";
 import { ReactComponent as IconHappy } from "./assets/icon-happy.svg";
 import { ReactComponent as IconJoy } from "./assets/icon-joy.svg";
 import { ReactComponent as IconSorrow } from "./assets/icon-sorrow.svg";
-import { ReactComponent as IconIndifference } from "./assets/icon-Indifference.svg";
+import { ReactComponent as IconIndifference } from "./assets/icon-indifference.svg";
 
 export {
     IconAddChat,
