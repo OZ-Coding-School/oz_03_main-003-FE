fb0d73d5dceec2f0ea4bacf98b5f7fafdafae514
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-07-16 18:13:54 +0900

    Feat: ModalTreeDetail.tsx 툴팁 컴포넌트 작성 #4

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,7 @@ import ModalDeleteChat from "./components/modal/ModalDeleteChat";
 import ModalUpdateChat from "./components/modal/ModalUpdateChat";
 import ModalCreateChat from "./components/modal/ModalCreateChat";
 import ModalCreateTree from "./components/modal/ModalCreateTree";
-import ButtonSignInGoogle from "./components/button/ButtonSignInGoogle";
-import ButtonSignInKakao from "./components/button/ButtonSignInKakao";
+import ModalTreeDetail from "./components/modal/ModalTreeDetail";
 function Layout() {
     return (
         <div className="font-body flex gap-4 p-10 w-full h-[200vh]">
@@ -45,6 +44,7 @@ function Layout() {
                     <BadgeSorrow />
                     <BadgeIndifference />
                     <BadgeWorry />
+                    <ModalTreeDetail />
                     <ModalDeleteChat />
                     <ModalUpdateChat />
                     <ModalCreateChat />
