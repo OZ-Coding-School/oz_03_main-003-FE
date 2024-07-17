f3fd19365c843086a036037b05f2db277a818b80
ungpyolee <uplee114@gmail.com>
2024-07-17 15:35:31 +0900

    Design: icon size와 color, margin 변경

--- src/Gnb.tsx
+++ src/Gnb.tsx
@@ -30,15 +30,17 @@ const Gnb = () => {
                         )}
                     >
                         <Link to={item.path} className="w-full flex items-center justify-center ">
-                            <IconComponent
-                                className={tw(
-                                    "w-5 h-5 mr-[5px]",
-                                    active
-                                        ? "fill-primary group-hover:fill-primary"
-                                        : "fill-gray-400 group-hover:fill-white"
-                                )}
-                                fill={active ? "#83DF00" : "#bebebe"}
-                            />
+                            <div className="w-5 h-5 mr-2 flex justify-center items-center">
+                                <IconComponent
+                                    className={tw(
+                                        "h-5",
+                                        active
+                                            ? "fill-primary group-hover:fill-primary"
+                                            : "fill-gray-200 group-hover:fill-white"
+                                    )}
+                                    fill={active ? "#83DF00" : "#bebebe"}
+                                />
+                            </div>
                             {item.label}
                             {active && (
                                 <span className="absolute bottom-0 left-0 w-full h-[2px] bg-primary" />
