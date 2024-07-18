c0ee09c8c9b6ef1082ce977293029547f95d0f02
ungpyolee <uplee114@gmail.com>
2024-07-18 18:47:00 +0900

    Feat : IconAddChat 클릭시 modal open 추가

--- src/pages/PageChat.tsx
+++ src/pages/PageChat.tsx
@@ -1,22 +1,34 @@
+import { useState } from "react";
 import { contentDummy } from "../components/chatList/ChatListDummy";
 import ChatListHeader from "../components/chatList/ChatListHeader";
 import ChatListItem from "../components/chatList/ChatListItem";
 import HeaderLoggedIn from "../components/header/HeaderLoggedIn";
+import ModalCreateChat from "../components/modal/ModalCreateChat";
 
 const PageChat = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
     return (
         <>
             <HeaderLoggedIn />
             <div className="bg-black pt-[129px] w-full h-screen box-border">
                 <div className="w-full h-full flex">
                     <div className="w-80 h-full border-r border-gray-600">
-                        <ChatListHeader />
+                        <ChatListHeader onAddChatClick={openModal} />
                         {contentDummy.map((item, index) => (
                             <ChatListItem key={index} item={item} />
                         ))}
                     </div>
                 </div>
             </div>
+            {isModalOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+                    <ModalCreateChat onClose={closeModal} />
+                </div>
+            )}
         </>
     );
 };
