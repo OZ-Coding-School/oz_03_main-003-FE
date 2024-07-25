42182adc8aa89f9f26ed691e4f226f9ca128bfaa
ungpyolee <uplee114@gmail.com>
2024-07-25 16:04:59 +0900

    Feat : 채팅방 Create, Read, Delete관련 연결 추가, 채팅방 생성시 더블클릭으로 두개 생성되는것 막기, 이름과 나무 유효성 검사 추가

--- src/pages/PageChat.tsx
+++ src/pages/PageChat.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
-import { contentDummy } from "../components/common/chatList/ChatListDummy";
 import ChatListHeader from "../components/common/chatList/ChatListHeader";
-import ChatListItem from "../components/common/chatList/ChatListItem";
 import HeaderLoggedIn from "../components/common/header/HeaderLoggedIn";
 import ModalCreateChat from "../components/common/modal/ModalCreateChat";
 import useUserInfo from "../hook/useInfo";
-// import { useUserStore } from "../config/store";
+import useChatRooms from "../hook/useChatRooms";
+import ChatListContent from "../components/common/chatList/ChatListContent";
 
 const PageChat = () => {
     const { getUserInfo } = useUserInfo();
+    const { fetchChatRooms } = useChatRooms();
 
     useEffect(() => {
         const refreshUserInfo = async () => {
@@ -17,10 +17,16 @@ const PageChat = () => {
         refreshUserInfo();
     }, [getUserInfo]);
 
+    useEffect(() => {
+        fetchChatRooms();
+    }, [fetchChatRooms]);
+
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const openModal = () => setIsModalOpen(true);
-    const closeModal = () => setIsModalOpen(false);
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 
     return (
         <>
@@ -29,9 +35,7 @@ const PageChat = () => {
                 <div className="w-full h-full flex">
                     <div className="w-80 h-full border-r border-gray-600">
                         <ChatListHeader onAddChatClick={openModal} />
-                        {contentDummy.map((item, index) => (
-                            <ChatListItem key={index} item={item} />
-                        ))}
+                        <ChatListContent />
                     </div>
                 </div>
             </div>
