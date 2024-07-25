d7b97c218266d639fc5eaf50187e7f4ac680cd59
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-07-25 16:29:29 +0900

    Fix: 계정 탈퇴버튼 문제 해결 #55

--- src/components/common/modal/ModalQuitSite.tsx
+++ src/components/common/modal/ModalQuitSite.tsx
@@ -28,6 +28,10 @@ const ModalQuitSite = ({ onClose }: ModalDeleteChatProps) => {
         nav("/");
     };
 
+    const stopPropagation = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.stopPropagation();
+    };
+
     useEffect(() => {
         if (ref.current) {
             ref.current.focus();
@@ -36,26 +40,29 @@ const ModalQuitSite = ({ onClose }: ModalDeleteChatProps) => {
 
     return (
         <>
-            <nav className="absolute opacity-50 top-0 w-full h-screen bg-black"></nav>
+            <nav
+                onClick={closeHandler}
+                className="z-10 absolute opacity-50 top-0 w-full h-screen bg-black"
+            ></nav>
             <motion.div
                 ref={ref}
                 tabIndex={-1}
-                initial={{ opacity: 0, scale: 0 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: [0, 1], scale: [0, 1] }}
                 transition={{ duration: 0.5, type: "spring" }}
+                onClick={closeHandler}
                 onKeyDown={(e) => {
                     e.key === "Escape" && closeHandler();
                 }}
-                className={tw("inset-0 select-none z-0 fixed flex items-center justify-center")}
+                className={tw("inset-0 select-none z-20 fixed flex items-center justify-center")}
             >
                 <nav
+                    onClick={stopPropagation}
                     className={tw(
                         "p-5 bg-gray-800 text-white w-80 border border-gray-600",
                         "absolute z-20"
                     )}
                 >
-                    <h3 className="font-title leading-5 mb-4 text-gray-200">대화 분석방 삭제</h3>
+                    <h3 className="font-title leading-5 mb-4 text-gray-200">계정 탈퇴</h3>
                     <p>계정 정보를 삭제하시겠습니까?</p>
                     <p className="text-literal-error text-xs">삭제 된 내용은 복구되지 않습니다</p>
                     <div className="text-right mt-2.5">
