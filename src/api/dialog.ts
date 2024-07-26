53ca12bf2e906a469b75554785b93d299bdd1c6d
ungpyolee <uplee114@gmail.com>
2024-07-26 21:43:10 +0900

    Feat : dialog api 호출 함수 추가

--- src/api/dialog.ts
+++ src/api/dialog.ts
@@ -1,5 +1,9 @@
 import { axiosInstance } from "./axios";
 
-export const sendUserMessage = (uuid: string) => {
-    return axiosInstance.post(`/dialog/message/user/${uuid}`);
+export const sendUserMessage = (uuid: string, message: string) => {
+    return axiosInstance.post(`/dialog/message/user/${uuid}`, { message });
+};
+
+export const getUserMessage = (uuid: string) => {
+    return axiosInstance.get(`/dialog/message/user/${uuid}`);
 };
