16ae6ba38271c8272175901ed194ebb323242c54
ungpyolee <uplee114@gmail.com>
2024-07-29 00:44:30 +0900

    Design : Badge 텍스트 색깔 변경

--- src/components/common/badge/BadgeAngry.tsx
+++ src/components/common/badge/BadgeAngry.tsx
@@ -5,12 +5,12 @@ const BadgeAngry = () => {
     return (
         <div
             className={tw(
-                "bg-literal-angry",
+                "bg-literal-angry text-black",
                 "px-4 w-24 h-10 font-title",
                 "flex items-center justify-center select-none"
             )}
         >
-            <IconAngry className="w-3.5 mr-2" />
+            <IconAngry className="w-3.5 mr-2 " />
             분노
         </div>
     );
--- src/components/common/badge/BadgeHappy.tsx
+++ src/components/common/badge/BadgeHappy.tsx
@@ -5,12 +5,12 @@ const BadgeHappy = () => {
     return (
         <div
             className={tw(
-                "bg-literal-happy",
+                "bg-literal-happy text-black",
                 "px-4 w-24 h-10 font-title",
                 "flex items-center justify-center select-none"
             )}
         >
-            <IconHappy className="w-5 mr-2" />
+            <IconHappy className="w-5 mr-2 " />
             기쁨
         </div>
     );
