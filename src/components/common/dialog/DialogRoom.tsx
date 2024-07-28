0495bba182c94c21a643f30ec0c860a236612d2f
ungpyolee <uplee114@gmail.com>
2024-07-28 23:34:23 +0900

    Feat : 1. 사용자 메시지보내기 2. ai 메시지 응답하기 3. 메시지 리스트 업데이트 4. 로딩구간 추가

--- src/components/common/dialog/DialogRoom.tsx
+++ src/components/common/dialog/DialogRoom.tsx
@@ -1,73 +1,71 @@
-import { useEffect, useRef } from "react";
-import { useUserChatStore } from "../../../config/store";
-import useFetchMessages from "../../../hook/useFetchMessage";
-import useFetchAIMessage from "../../../hook/useFetchAIMessage";
+import { useRef, useEffect } from "react";
+import { useUserChatStore, useUserStore } from "../../../config/store";
 import ButtonEmoTree from "../button/ButtonEmoTree";
+import useGetDialogList from "../../../hook/useGetDialogList";
+
 interface DialogRoomProps {
     chatRoomUuid: string;
 }
-
 const DialogRoom = ({ chatRoomUuid }: DialogRoomProps) => {
-    const { userMessages, aiResponses } = useUserChatStore((state) => ({
-        userMessages: state.userMessages[chatRoomUuid] || [],
-        aiResponses: state.aiResponses[chatRoomUuid] || [],
-    }));
-    const { fetchMessages } = useFetchMessages();
-    const { fetchAIMessage } = useFetchAIMessage();
+    const { dialogList, isLoading, error } = useGetDialogList(chatRoomUuid);
+    const { chatRooms } = useUserChatStore();
+    const { userData } = useUserStore();
+    const treeUuid = chatRooms.find((data) => data.chat_room_uuid === chatRoomUuid)?.tree_uuid;
+    const treeName = userData.treeDetail.find((t) => t.tree_uuid === treeUuid)?.tree_name;
 
     const messagesEndRef = useRef<HTMLDivElement>(null);
 
     const scrollToBottom = () => {
-        if (messagesEndRef.current) {
-            messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
-        }
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     };
 
     useEffect(() => {
-        fetchMessages(chatRoomUuid);
-    }, [chatRoomUuid, fetchMessages]);
-
-    useEffect(() => {
-        if (userMessages.length > 0) {
-            const lastMessage = userMessages[userMessages.length - 1];
-            fetchAIMessage(chatRoomUuid, lastMessage.message_uuid);
-        }
-    }, [chatRoomUuid, userMessages, fetchAIMessage]);
-
-    useEffect(() => {
         scrollToBottom();
-    }, [userMessages, aiResponses]);
+    }, [dialogList]);
 
+    if (isLoading) return <div>로딩 중...</div>;
+    if (error) return <div>에러 발생: {error}</div>;
     return (
         <div className="w-full h-full text-white overflow-y-auto">
-            {userMessages.map((msg, index) => (
-                <div key={msg.message_uuid}>
-                    <div className="p-8 bg-gray-800 rounded-md mb-4 w-10/12 ml-auto mr-10">
-                        {msg.message}
-                    </div>
-                    {aiResponses[index] && (
-                        <div className="rounded-md mb-10">
-                            <div className="p-8 border-b border-gray-800">
-                                <p className="text-gray-200  text-lg font-title mb-4">
-                                    대화 속의 감정을 요약했습니다
-                                </p>
-                                {aiResponses[index].message}
-                            </div>
-                            <div className="p-8 border-b border-gray-800">
-                                <p className="text-gray-200  text-lg font-title mb-6">
-                                    감정 키워드 !
+            <div>
+                {dialogList.length === 0 ? (
+                    <div>대화 기록이 없습니다.</div>
+                ) : (
+                    dialogList.map((dialogItem, index) => (
+                        <div key={index}>
+                            <div className="mb-4 mr-8">
+                                <p className="p-8 bg-gray-800 rounded-md w-10/12 ml-auto">
+                                    {dialogItem.userMessage.message}
                                 </p>
                             </div>
-                            <div className="p-8 border-b border-gray-600 pb-10">
-                                <p className="text-gray-200  text-lg font-title mb-4">
-                                    감정을 전달하면 나무가 성장합니다!
-                                </p>
-                                <ButtonEmoTree>감정을 나무에 전달하기</ButtonEmoTree>
-                            </div>
+                            {dialogItem.aiMessage && (
+                                <div className="mb-10">
+                                    <div className="p-8 border-b border-gray-800">
+                                        <p className="text-gray-200 text-lg font-title mb-4">
+                                            대화 속의 감정을 요약했습니다
+                                        </p>
+                                        {dialogItem.aiMessage.message}
+                                    </div>
+                                    <div className="p-8 border-b border-gray-800">
+                                        <p className="text-gray-200 text-lg font-title mb-6">
+                                            감정 키워드 !
+                                        </p>
+                                        {JSON.stringify(dialogItem.aiMessage.sentiments)}
+                                    </div>
+                                    <div className="p-8 border-b border-gray-600 pb-10">
+                                        <p className="text-gray-200 text-lg font-title mb-4">
+                                            감정을 전달하면{" "}
+                                            <span className="text-primary">{treeName}</span>
+                                            나무가 성장합니다!
+                                        </p>
+                                        <ButtonEmoTree>감정을 나무에 전달하기</ButtonEmoTree>
+                                    </div>
+                                </div>
+                            )}
                         </div>
-                    )}
-                </div>
-            ))}
+                    ))
+                )}
+            </div>
             <div ref={messagesEndRef} />
         </div>
     );
