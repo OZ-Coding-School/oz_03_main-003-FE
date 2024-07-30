e9858c57e5b39a9afbe43f3560740d646d92302d
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-07-30 17:22:00 +0900

    Style: 랜딩페이지 이모트리 버튼 로직 #87

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,7 +1,24 @@
 import { motion } from "framer-motion";
 import { IconLineStar } from "../config/IconData";
-import ButtonEmoTree from "./common/button/ButtonEmoTree";
+import ButtonEmoTreeSkeleton from "./common/button/ButtonEmoTreeSkeleton";
+import { TREE_IMG } from "../config/const";
+import useSound from "use-sound";
+import btnSendTree from "../assets/sound/btn_sendTree.mp3";
+import { useState } from "react";
+
 const Landing = () => {
+    const [playTree] = useSound(btnSendTree, { volume: 0.75 });
+    const [index, setIndex] = useState(0);
+
+    const treeChangeHandler = () => {
+        playTree();
+        if (index === 5) {
+            setIndex(0);
+        } else {
+            setIndex((prev) => prev + 1);
+        }
+    };
+
     return (
         <>
             <div className="text-white w-full">
@@ -74,23 +91,30 @@ const Landing = () => {
                                     대화 속의 감정을 나무에 기록하고 나무를 성장시키세요 !
                                 </p>
                             </div>
-                            <div className="text-center mt-[113px]">
-                                <div>
+                            <div className="text-center mt-[113px] relative">
+                                <div className="">
                                     <strong className="font-title text-[14px] text-primary">
                                         새싹
                                     </strong>
                                     <p className="mt-[2px]">지훈의 감정</p>
                                 </div>
-                                <div className="mt-[70px]">
+                                <div className="mt-[105px] w-full relative flex flex-col items-center animate-gravity">
+                                    <motion.img
+                                        className="absolute w-[200px] -top-[110px]"
+                                        src={TREE_IMG[index]}
+                                    ></motion.img>
                                     <img
                                         src="../src/assets/images/landing-ground.svg"
                                         alt="땅"
                                         className="m-auto"
                                     />
                                 </div>
-                                <ButtonEmoTree className="mt-10 mb-5">
+                                <ButtonEmoTreeSkeleton
+                                    onClick={treeChangeHandler}
+                                    className="mt-10 mb-5"
+                                >
                                     감정을 나무에 전달하기
-                                </ButtonEmoTree>
+                                </ButtonEmoTreeSkeleton>
                                 <p>전달하기 버튼을 클릭해보세요.</p>
                             </div>
                         </div>
---
+++ src/components/common/button/ButtonEmoTreeSkeleton.tsx
@@ -0,0 +1,62 @@
+import { motion } from "framer-motion";
+import { PropsWithChildren, useState } from "react";
+import { twMerge as tw } from "tailwind-merge";
+import useSound from "use-sound";
+import btnSendTree from "../../../assets/sound/btn_sendTree.mp3";
+
+interface ButtonProps extends PropsWithChildren {
+    className?: string;
+    type?: "submit" | "reset" | "button";
+    onClick?: () => void;
+}
+
+const ButtonEmoTreeSkeleton = ({ className, type = "button", children, onClick }: ButtonProps) => {
+    const [hovered, setHovered] = useState(false);
+
+    return (
+        <>
+            <motion.button
+                whileHover={{
+                    backgroundPosition: "100% 0",
+                    transition: { duration: 0.5 },
+                }}
+                type={type}
+                onClick={onClick}
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+                className={tw(
+                    "font-title text-base select-none text-white text-center items-center",
+                    "w-[220px] h-[48px] bg-gradient-to-l relative z-0",
+                    "bg-gradient-to-r from-[#B88BFF] via-[#FFB4A2] to-[#8BFFB2]",
+                    "bg-[length:200%_100%] border border-white",
+                    className
+                )}
+            >
+                <div
+                    style={{
+                        textShadow: "0 2px 5px rgba(0, 0, 0, 0.5)",
+                    }}
+                    className="absolute w-full h-full top-0 left-0 flex justify-center items-center z-10"
+                >
+                    {children}
+                </div>
+                <img
+                    className="absolute w-[37px] h-[43px] z-20 -top-2 -left-1 animate-bounce"
+                    src="/icon-stars.png"
+                    loading="lazy"
+                    style={{
+                        filter: "drop-shadow(0 2px 5px rgba(0, 0, 0, 0.5))",
+                    }}
+                ></img>
+                <div
+                    className={tw(
+                        "w-full transition h-full absolute bg-black z-0 top-0 left-0",
+                        hovered ? "opacity-15" : "opacity-60"
+                    )}
+                ></div>
+            </motion.button>
+        </>
+    );
+};
+
+export default ButtonEmoTreeSkeleton;
