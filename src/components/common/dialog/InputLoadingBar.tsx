b2c41e32a8f22b7a9821c49dee80d5deac5e24ad
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-07-31 19:25:35 +0900

    Fix: 로딩 바 텍스트 추가

--- src/components/common/dialog/InputLoadingBar.tsx
+++ src/components/common/dialog/InputLoadingBar.tsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const InputLoadingBar = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const loaderVariants = {
         start: {
             x: 0,
@@ -10,8 +12,23 @@ const InputLoadingBar = () => {
         },
     };
 
+    useEffect(() => {
+        const time = setTimeout(() => {
+            setIsLoading(true);
+        }, 5000);
+
+        return () => {
+            clearTimeout(time);
+        };
+    });
+
     return (
         <div className="w-full absolute flex justify-center bottom-[150px] z-50">
+            {isLoading && (
+                <p className="text-gray-200 animate-pulse">
+                    대화내용을 분석하고 있습니다 잠시만 기다려주세요
+                </p>
+            )}
             <div className="rounded-full overflow-hidden bg-slate-800 w-[200px] h-[5px]">
                 <motion.div
                     className="h-full w-[20px] rounded-md bg-primary"
