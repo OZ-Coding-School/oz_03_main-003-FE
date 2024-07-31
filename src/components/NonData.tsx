f205fbc3f7bb41788830834288648206c453e188
ungpyolee <uplee114@gmail.com>
2024-07-31 16:56:14 +0900

    Design : font 자간 변경과 이미지 사이즈변경

--- src/components/NonData.tsx
+++ src/components/NonData.tsx
@@ -2,8 +2,8 @@ const NonData = () => {
     return (
         <div className="select-none w-full h-[100vh] bg-black flex flex-col justify-center items-center">
             <div className="flex flex-col justify-center items-center h-full">
-                <div className="text-2xl text-white mb-10 font-bold">Tree Not Found</div>
-                <div className="text-xl text-white mb-20">
+                <div className="text-2xl text-white mb-4 font-bold">Tree Not Found</div>
+                <div className="text-lg text-white mb-20">
                     나무를 먼저 추가하고 채팅방을 생성해주세요
                 </div>
 
