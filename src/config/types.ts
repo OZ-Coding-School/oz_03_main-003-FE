1d9cbdb0a30ae2dc549fad5ea0582b93a0e87c07
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-08-01 23:39:29 +0900

    Feat: 자동 채팅방 열림 기능 추가

--- src/config/types.ts
+++ src/config/types.ts
@@ -155,8 +155,8 @@ export interface ChatRoom {
     chat_room_uuid: string;
     chat_room_name: string;
     tree_uuid: string;
-    created_at: string;
-    updated_at: string;
+    created_at?: string;
+    updated_at?: string;
 }
 
 export interface UpdateChatRoom {
